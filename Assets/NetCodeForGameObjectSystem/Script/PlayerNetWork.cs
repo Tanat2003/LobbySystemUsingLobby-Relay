b2386e3cee5c7f5f41509f685c4c61759e9457cb
Tanat seelueang <tanatdeveloper@gmail.com>
2025-07-02 17:15:43 +0700

    Adding player controll

--- Assets/NetCodeForGameObjectSystem/Script/PlayerNetWork.cs
+++ Assets/NetCodeForGameObjectSystem/Script/PlayerNetWork.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -7,15 +8,38 @@ public class PlayerNetWork : NetworkBehaviour
 {
     //ทุกอย่างที่ใช้networkอย่าinitilizeในawakeกับStart,Update ให้ใช้ OnNetWorkSpawn();แทน
     //netWork มีแค่serverที่สามารถรันคำสั่งได้ถ้าclientต้องการรัน แต่เปลี่ยนค่าnetworkVariableค่าdefaultมันจะเซ็ตได้จากserver(สามารถเปลี่ยนได้)
-   
-    
-    private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1,NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner); //variableที่เราจะใช้ส่งข้อมูลผ่านNetWorkcแบบส่งให้ทั้งclientและอื่นๆไรงี้ห้ามเป็นค่าnull ต้องinitializedหรือกำหนดค่ามัน
 
+
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>
+        (new MyCustomData
+        {
+            _int = 50,
+            _bool = true,
+
+        },
+        NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner); //variableที่เราจะใช้ส่งข้อมูลผ่านNetWorkcแบบส่งให้ทั้งclientและอื่นๆไรงี้ห้ามเป็นค่าnull ต้องinitializedหรือกำหนดค่ามัน
+
+    [SerializeField] private Transform spawnObjectPrefab;
+    
+    //structส่งผ่านNetWorkไม่ได้ต้องimplement INetworkSerializableเพื่อreferenceค่าด้วย
+    public struct MyCustomData: INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message; //เวลาส่งข้อมูลstringผ่านnetwork มันต้องกำหนดขนาดข้อมูลด่วยเราเลยใช้fixedstring
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
     public override void OnNetworkSpawn() //เฉพาะตอนที่NetWorkSpawn
     {
-        randomNumber.OnValueChanged += (int previousValue, int newValue) =>
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
         {
-
+            Debug.Log("Owner: " + OwnerClientId + "has Create " + newValue._int);
         };
     }
 
@@ -24,6 +48,11 @@ public class PlayerNetWork : NetworkBehaviour
     {
         if (!IsOwner) //เฉพาะOwnerของobjนั้นๆที่มีสคริปต์นี้ติดถึงรันได้
             return;
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+          Transform spawnObjectTransform  = Instantiate(spawnObjectPrefab);
+            spawnObjectTransform.GetComponent<NetworkObject>().Spawn(true); // SpawnในNetWork และnetworkobjectสามารถspawnในnetworkServerเท่านั้น
+        }
 
         Vector3 moveDir = new Vector3(0, 0, 0);
         if(Input.GetKeyDown(KeyCode.W))
@@ -42,4 +71,6 @@ public class PlayerNetWork : NetworkBehaviour
         float moveSPD = 5f;
         transform.position += moveDir * moveSPD * Time.deltaTime;
     }
+
+    
 }
